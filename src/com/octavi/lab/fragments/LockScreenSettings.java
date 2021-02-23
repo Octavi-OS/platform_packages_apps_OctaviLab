9bf88bce026ef37910e66fd3e1983da5e660e188
Sagarrokade006 <Sagarrokade006@gmail.com>
2021-02-23 08:34:32 +0000

    Remove FOD-Customizations

--- src/com/octavi/lab/fragments/LockScreenSettings.java
+++ src/com/octavi/lab/fragments/LockScreenSettings.java
@@ -41,15 +41,9 @@ import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import com.android.internal.util.octavi.OctaviUtils;
-
 public class LockScreenSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String KEY_FOD_RECOGNIZING_ANIMATION = "fod_recognizing_animation";
-    private static final String KEY_FOD_RECOGNIZING_ANIMATION_LIST = "fod_recognizing_animation_list";
-    private static final String FOD_GESTURE = "fod_gesture";
-    private static final String FOD_ANIMATION_CATEGORY = "fod_animations";
     private static final String LOCKSCREEN_MAX_NOTIF_CONFIG = "lockscreen_max_notif_cofig";
 
     private CustomSeekBarPreference mMaxKeyguardNotifConfig;
@@ -61,32 +55,14 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
 
         ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
-        final PreferenceCategory fodCat = (PreferenceCategory) prefScreen
-                .findPreference(FOD_ANIMATION_CATEGORY);
-
-        final boolean isFodAnimationResources = OctaviUtils.isPackageInstalled(getContext(),
-                      getResources().getString(com.android.internal.R.string.config_fodAnimationPackage));
 
         Resources resources = getResources();
-
-        SystemSettingSwitchPreference mFODSwitchPref = (SystemSettingSwitchPreference) findPreference(KEY_FOD_RECOGNIZING_ANIMATION);
-	SystemSettingListPreference mFODListViewPref = (SystemSettingListPreference) findPreference(KEY_FOD_RECOGNIZING_ANIMATION_LIST);
-        SystemSettingSwitchPreference mScreenOffFODSwitchPref = (SystemSettingSwitchPreference) findPreference(FOD_GESTURE);
 
         mMaxKeyguardNotifConfig = (CustomSeekBarPreference) findPreference(LOCKSCREEN_MAX_NOTIF_CONFIG);
         int kgconf = Settings.System.getInt(getContentResolver(),
                 Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 3);
         mMaxKeyguardNotifConfig.setValue(kgconf);
         mMaxKeyguardNotifConfig.setOnPreferenceChangeListener(this);
-
-	if (!resources.getBoolean(R.bool.config_showFODAnimationSettings) && !resources.getBoolean(R.bool.config_showScreenoffFOD)){
-	    prefScreen.removePreference(mFODSwitchPref);
-            prefScreen.removePreference(mFODListViewPref);
-            prefScreen.removePreference(mScreenOffFODSwitchPref);
-	}
-        if (!isFodAnimationResources) {
-            prefScreen.removePreference(fodCat);
-        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
