2e92e7e50ce25a0ceeb8c5c5cb008dbf8114e89c
Rahul <rahul731999@gmail.com>
2021-05-13 22:22:53 +0530

    Lab: move Scrolling cache to Misc

--- src/com/octavi/lab/fragments/MiscSettings.java
+++ src/com/octavi/lab/fragments/MiscSettings.java
@@ -33,7 +33,11 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
     private static final String KEY_PULSE_BRIGHTNESS = "ambient_pulse_brightness";
     private static final String KEY_DOZE_BRIGHTNESS = "ambient_doze_brightness";
+    private static final String SCROLLINGCACHE_PREF = "pref_scrollingcache";
+    private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
+    private static final String SCROLLINGCACHE_DEFAULT = "2";
 
+    private ListPreference mScrollingCachePref;
     private CustomSeekBarPreference mPulseBrightness;
     private CustomSeekBarPreference mDozeBrightness;
 
@@ -51,6 +55,11 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         if (defaultPulse == -1) {
             defaultPulse = defaultDoze;
         }
+
+        mScrollingCachePref = (ListPreference) findPreference(SCROLLINGCACHE_PREF);
+        mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
+                SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP, SCROLLINGCACHE_DEFAULT)));
+        mScrollingCachePref.setOnPreferenceChangeListener(this);
 
         mPulseBrightness = (CustomSeekBarPreference) findPreference(KEY_PULSE_BRIGHTNESS);
         int value = Settings.System.getInt(getContentResolver(),
@@ -78,7 +87,12 @@ public class MiscSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(),
                     Settings.System.OMNI_DOZE_BRIGHTNESS, value);
             return true;
-         }
+         } else if (preference == mScrollingCachePref) {
+            if (newValue != null) {
+                SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, (String) newValue);
+            }
+            return true;
+        }
         return false;
     }
 
