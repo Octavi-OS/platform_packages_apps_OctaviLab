4edfa576be1ae2c7a5d24de7664f8b83ef39d4d8
Sagarrokade006 <Sagarrokade006@gmail.com>
2021-06-15 01:25:25 +0000

    Revert "Introduce RGB accenter [2/2]"
    This reverts commit b2f623ded649f6fbec6c3aedb593809f126bdc54

--- src/com/octavi/lab/fragments/OctaviThemes.java
+++ src/com/octavi/lab/fragments/OctaviThemes.java
@@ -48,12 +48,7 @@ public class OctaviThemes extends DashboardFragment implements
 
     private static final String TAG = "OctaviThemes";
 
-    private static final String ACCENT_COLOR = "accent_color";
-    private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
     private static final String SWITCH_STYLE = "switch_style";
-
-    private IOverlayManager mOverlayService;
-    private ColorPickerPreference mThemeColor;
 
     private ListPreference mSwitchStyle;
 
@@ -71,9 +66,6 @@ public class OctaviThemes extends DashboardFragment implements
         super.onCreate(icicle);
 
         //addPreferencesFromResource(R.xml.octavi_themes);
-        mOverlayService = IOverlayManager.Stub
-                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
-        setupAccentPref();
 
         mSwitchStyle = (ListPreference) findPreference(SWITCH_STYLE);
         int switchStyle = Settings.System.getInt(getContext().getContentResolver(),
@@ -86,33 +78,13 @@ public class OctaviThemes extends DashboardFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mThemeColor) {
-            int color = (Integer) objValue;
-            String hexColor = String.format("%08X", (0xFFFFFFFF & color));
-            SystemProperties.set(ACCENT_COLOR_PROP, hexColor);
-            try {
-                 mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
-                 mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
-                 mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
-             } catch (RemoteException ignored) {
-             }
-        } else if (preference == mSwitchStyle) {
+        if (preference == mSwitchStyle) {
                 String value = (String) objValue;
                 Settings.System.putInt(getContext().getContentResolver(), Settings.System.SWITCH_STYLE, Integer.valueOf(value));
                 int valueIndex = mSwitchStyle.findIndexOfValue(value);
                 mSwitchStyle.setSummary(mSwitchStyle.getEntries()[valueIndex]);
 	}
         return true;
-    }
-
-    private void setupAccentPref() {
-        mThemeColor = (ColorPickerPreference) findPreference(ACCENT_COLOR);
-        String colorVal = SystemProperties.get(ACCENT_COLOR_PROP, "-1");
-        int color = "-1".equals(colorVal)
-                ? Color.WHITE
-                : Color.parseColor("#" + colorVal);
-        mThemeColor.setNewPreviewColor(color);
-        mThemeColor.setOnPreferenceChangeListener(this);
     }
 
     @Override
