a1d85d96bb315dc12d189e93c47d832608554133
SuperDroidBond <SuperDroidBond@users.noreply.github.com>
2021-06-17 12:17:52 +0000

    Separate LS UI from other LS elements

--- src/com/octavi/lab/fragments/LockScreenSettings.java
+++ src/com/octavi/lab/fragments/LockScreenSettings.java
@@ -60,22 +60,9 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
     private static final String LOCKSCREEN_FOD_CATEGORY = "lockscreen_fod_category";
     private static final String AMBIENT_ICONS_COLOR = "ambient_icons_color";
     private static final String AMBIENT_ICONS_LOCKSCREEN = "ambient_icons_lockscreen";
-    private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
-    private static final String LOCK_DATE_FONTS = "lock_date_fonts";
-    private static final String CLOCK_FONT_SIZE  = "lockclock_font_size";
-    private static final String DATE_FONT_SIZE  = "lockdate_font_size";
-    private static final String LOCK_OWNERINFO_FONTS = "lock_ownerinfo_fonts";
-    private static final String LOCKOWNER_FONT_SIZE = "lockowner_font_size";
 
     private CustomSeekBarPreference mMaxKeyguardNotifConfig;
-    private CustomSeekBarPreference mClockFontSize;
-    private CustomSeekBarPreference mDateFontSize;
-    private CustomSeekBarPreference mOwnerInfoFontSize;
     private ColorPickerPreference mAmbientIconsColor;
-    private ListPreference mLockClockFonts;
-    private ListPreference mLockDateFonts;
-    private ListPreference mLockOwnerInfoFonts;
-    private CardPreference mLockscreenFod;
     private SystemSettingSwitchPreference mAmbientIconsLockscreen;
 
     private ContentResolver mResolver;
@@ -117,45 +104,6 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         mAmbientIconsColor.setNewPreviewColor(intColor);
         mAmbientIconsColor.setSummary(hexColor);
         mAmbientIconsColor.setOnPreferenceChangeListener(this);
-
-        // Lockscren Clock Fonts
-        mLockClockFonts = (ListPreference) findPreference(LOCK_CLOCK_FONTS);
-        mLockClockFonts.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.LOCK_CLOCK_FONTS, 34)));
-        mLockClockFonts.setSummary(mLockClockFonts.getEntry());
-        mLockClockFonts.setOnPreferenceChangeListener(this);
-
-        // Lockscren Date Fonts
-        mLockDateFonts = (ListPreference) findPreference(LOCK_DATE_FONTS);
-        mLockDateFonts.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.LOCK_DATE_FONTS, 32)));
-        mLockDateFonts.setSummary(mLockDateFonts.getEntry());
-        mLockDateFonts.setOnPreferenceChangeListener(this);
-
-        // Lock Clock Size
-        mClockFontSize = (CustomSeekBarPreference) findPreference(CLOCK_FONT_SIZE);
-        mClockFontSize.setValue(Settings.System.getInt(getContentResolver(),
-                Settings.System.LOCKCLOCK_FONT_SIZE, 78));
-        mClockFontSize.setOnPreferenceChangeListener(this);
-
-        // Lock Date Size
-        mDateFontSize = (CustomSeekBarPreference) findPreference(DATE_FONT_SIZE);
-        mDateFontSize.setValue(Settings.System.getInt(getContentResolver(),
-                Settings.System.LOCKDATE_FONT_SIZE, 18));
-        mDateFontSize.setOnPreferenceChangeListener(this);
-
-        // Lockscren OwnerInfo Fonts
-        mLockOwnerInfoFonts = (ListPreference) findPreference(LOCK_OWNERINFO_FONTS);
-        mLockOwnerInfoFonts.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.LOCK_OWNERINFO_FONTS, 0)));
-        mLockOwnerInfoFonts.setSummary(mLockOwnerInfoFonts.getEntry());
-        mLockOwnerInfoFonts.setOnPreferenceChangeListener(this);
-
-        // Lockscren OwnerInfo Size
-        mOwnerInfoFontSize = (CustomSeekBarPreference) findPreference(LOCKOWNER_FONT_SIZE);
-        mOwnerInfoFontSize.setValue(Settings.System.getInt(getContentResolver(),
-                Settings.System.LOCKOWNER_FONT_SIZE,21));
-        mOwnerInfoFontSize.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -178,39 +126,6 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
             int intHex = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putInt(resolver,
                     Settings.System.AMBIENT_ICONS_COLOR, intHex);
-            return true;
-        } else if (preference == mLockClockFonts) {
-            Settings.System.putInt(getContentResolver(), Settings.System.LOCK_CLOCK_FONTS,
-                    Integer.valueOf((String) newValue));
-            mLockClockFonts.setValue(String.valueOf(newValue));
-            mLockClockFonts.setSummary(mLockClockFonts.getEntry());
-            return true;
-        } else if (preference == mLockDateFonts) {
-            Settings.System.putInt(getContentResolver(), Settings.System.LOCK_DATE_FONTS,
-                    Integer.valueOf((String) newValue));
-            mLockDateFonts.setValue(String.valueOf(newValue));
-            mLockDateFonts.setSummary(mLockDateFonts.getEntry());
-            return true;
-        } else if (preference == mClockFontSize) {
-            int top = (Integer) newValue;
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.LOCKCLOCK_FONT_SIZE, top*1);
-            return true;
-        } else if (preference == mDateFontSize) {
-            int top = (Integer) newValue;
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.LOCKDATE_FONT_SIZE, top*1);
-            return true;
-        } else if (preference == mLockOwnerInfoFonts) {
-            Settings.System.putInt(getContentResolver(), Settings.System.LOCK_OWNERINFO_FONTS,
-                    Integer.valueOf((String) newValue));
-            mLockOwnerInfoFonts.setValue(String.valueOf(newValue));
-            mLockOwnerInfoFonts.setSummary(mLockOwnerInfoFonts.getEntry());
-            return true;
-        } else if (preference == mOwnerInfoFontSize) {
-            int top = (Integer) newValue;
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.LOCKOWNER_FONT_SIZE, top*1);
             return true;
         }
         return false;
