b09a0736fdbea6b8c11689478e9bafb70009660f
Sagarrokade006 <Sagarrokade006@gmail.com>
2021-12-11 01:50:02 +0000

    Move Themeing to OctaviLab

--- src/com/octavi/lab/fragments/OctaviThemes.java
+++ src/com/octavi/lab/fragments/OctaviThemes.java
@@ -16,28 +16,49 @@
 
 package com.octavi.lab.fragments;
 
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Color;
+import com.android.internal.logging.nano.MetricsProto;
+
 import android.os.Bundle;
-import android.os.Handler;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
-import android.provider.Settings;
+import com.android.settings.development.OverlayCategoryPreferenceController;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.content.res.Resources;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
-
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.SettingsPreferenceFragment;
-
-import com.octavi.support.colorpicker.ColorPickerPreference;
-import com.octavi.lab.preferences.CustomSeekBarPreference;
+import android.provider.Settings;
+import com.octavi.support.preference.SystemSettingMasterSwitchPreference;
 import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.content.Context;
+import androidx.fragment.app.Fragment;
+import android.view.View;
+import android.os.UserHandle;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import android.util.Log;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 
-public class OctaviThemes extends SettingsPreferenceFragment
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.regex.Pattern;
+
+public class OctaviThemes extends DashboardFragment
             implements Preference.OnPreferenceChangeListener  {
 
     private static final String TAG = "OctaviThemes";
@@ -46,11 +67,34 @@ public class OctaviThemes extends SettingsPreferenceFragment
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.octavi_themes);
-
         PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
 
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.octavi_themes;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.font"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.icon_pack"));
+        return controllers;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
     }
 
     @Override
